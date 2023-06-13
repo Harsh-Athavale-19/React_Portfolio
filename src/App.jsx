931b414b6de23d9e0405a706913e9d6ff6fd7bba
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2023-06-13 13:43:46 +0530

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,34 @@
+import "../../assets/css/common.css";
+import profileImage from "../../assets/images/michael.jpg";
+import "./Home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="home" id="home">
+        <div className="container-fluid background_image">
+          <div className="row">
+            {/* INFO COLUMN */}
+            <div className="col-12 col-lg-6 hero_wrapper order-2 order-lg-1">
+              <div className="hero_text">
+                <h2>Hi, There</h2>
+                <h1>I&#39;m HARSH ATHAVALE</h1>
+                <h2>
+                  I am into <span>Frontend Development</span>
+                </h2>
+              </div>
+            </div>
+
+            {/* IMAGE COLUMN */}
+            <div className="col-12 col-lg-6 img_wrapper order-1 order-lg-2">
+              <div className="hero_img">
+                <img src={profileImage} alt="profileImage" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
